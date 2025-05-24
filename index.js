3a32ec5ff1089219bdf39aa194efa138fe712737
draxdev <davipeterson173@gmail.com>
2025-05-24 15:17:25 -0400

    fix: alter port

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-//branch dev
