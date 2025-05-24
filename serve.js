f6867f770f5ea78a35b6518747ae7e1c2f4f69f7
william <williambreno666@gmail.com>
2025-05-24 15:00:28 -0400

    coisando o coisa

---
+++ serve.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000;
